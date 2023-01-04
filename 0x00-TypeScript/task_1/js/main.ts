a5e79aab720b5eaf6063c57d4d880329169cd08f
saifelleuch <sa2014if@gmail.com>
2023-01-04 12:14:44 +0100

    another one

--- 0x00-TypeScript/task_1/js/main.ts
+++ 0x00-TypeScript/task_1/js/main.ts
@@ -1,25 +1,48 @@
 interface Teacher {
-    readonly firstName: string,
-    readonly lastName: string,
-    fullTimeEmployee: boolean,
-    yearsOfExperience?: number,
-    location: string,
-    [possName: string]: any;
+	readonly firstName: string,
+	readonly lastName: string,
+	fullTimeEmployee: boolean,
+	yearsOfExperience?: number,
+	location: string,
+	[possName: string]: any;
 };
 
 interface Directors extends Teacher {
-    numberOfReports: number;
+	numberOfReports: number;
 };
 
 interface printTeacherFunction {
-    (firstName: string, lastName: string): string;
+	(firstName: string, lastName: string): string;
 };
 
 export const printTeacher: printTeacherFunction = function(
-    firstName: string,
-    lastName: string
-    ): string {
-    let firstLetter: string = firstName.charAt(0);
-    return `${firstLetter}. ${lastName}`
+	firstName: string,
+	lastName: string
+	): string {
+	let firstLetter: string = firstName.charAt(0);
+	return `${firstLetter}. ${lastName}`
 };
 
+interface StudentClassConstructor {
+	new(firstName: string, lastName: string): StudentClassInterface;
+}
+
+interface StudentClassInterface {
+	firstName: string;
+	lastName: string;
+}
+
+class StudentClass implements StudentClassInterface {
+	firstName: string;
+	lastName: string;
+	constructor(firstName: string, lastName: string) {
+			this.firstName = firstName;
+			this.lastName = lastName;
+	}
+	workOnHomework() {
+			return 'Currently working';
+	}
+	displayName() {
+			return (`${this.firstName}`);
+	}
+}
