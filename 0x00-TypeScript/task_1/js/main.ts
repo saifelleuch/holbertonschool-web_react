5294f10f42c788ade6765897ea34723e91c5f092
saifelleuch <sa2014if@gmail.com>
2023-01-04 12:01:57 +0100

    print teacher

--- 0x00-TypeScript/task_1/js/main.ts
+++ 0x00-TypeScript/task_1/js/main.ts
@@ -10,3 +10,16 @@ interface Teacher {
 interface Directors extends Teacher {
     numberOfReports: number;
 };
+
+interface printTeacherFunction {
+    (firstName: string, lastName: string): string;
+};
+
+export const printTeacher: printTeacherFunction = function(
+    firstName: string,
+    lastName: string
+    ): string {
+    let firstLetter: string = firstName.charAt(0);
+    return `${firstLetter}. ${lastName}`
+};
+
