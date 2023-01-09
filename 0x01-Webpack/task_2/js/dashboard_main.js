a68bc60b31128deb00c0051cda278c9c4ac299e5
saifelleuch <sa2014if@gmail.com>
2023-01-09 14:50:09 +0100

    another one

---
+++ 0x01-Webpack/task_2/js/dashboard_main.js
@@ -0,0 +1,20 @@
+import '../css/main.css';
+import $ from "jquery";
+import _ from 'lodash';
+
+$("body").append("<p>Holberton Dashboard</p>");
+$("body").append("<p>Dashboard data for the students</p>");
+$('body').append('<button>Click here to get started</button>');
+$("body").append("<p id='count'></p>");
+$("body").append("<p>Copyright - Holberton School</p>");
+
+const updateCounter = () => {
+    let nbr_btn_click = $('#count').html() || 0;
+    $('button').on('click', () => {
+        nbr_btn_click++;
+        $('#count').html(`${nbr_btn_click} clicks on the button`);
+    });
+};
+
+_.debounce(updateCounter, 500);
+updateCounter();
