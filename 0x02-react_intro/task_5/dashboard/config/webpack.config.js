c18338703b3b6b7b2ab92b4fb5a242651cbc6146
saifelleuch <sa2014if@gmail.com>
2023-01-11 22:08:55 +0100

    another one

--- 0x02-react_intro/task_5/dashboard/config/webpack.config.js
+++ 0x02-react_intro/task_5/dashboard/config/webpack.config.js
@@ -8,4 +8,18 @@ module.exports = {
     filename: 'bundle.js',
     path: path.resolve('./dist'),
   },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/i,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  }
 };
