055d002fe13d56d5f2d6b3e1e384e305407b3a4c
saifelleuch <sa2014if@gmail.com>
2023-01-16 01:02:51 +0100

    another one

---
+++ 0x03-react_props/dashboard/task_0/config/webpack.config.js
@@ -0,0 +1,48 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  devtool: 'inline-source-map',
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve('./dist'),
+  },
+  devServer: {
+    hot: true,
+    contentBase: path.resolve('./dist'),
+    compress: true,
+    port: 3000,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(jpg|png)$/i,
+        use: [
+          'file-loader',
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              bypassOnDebug: true,
+              disable: true,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.jsx?$/i,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        },
+      },
+    ],
+  },
+};
