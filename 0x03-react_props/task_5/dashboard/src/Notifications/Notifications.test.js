e0fb5c5db27e3277fe14c6ee252aaeb4716c6548
saifelleuch <sa2014if@gmail.com>
2023-01-17 22:59:02 +0100

    another one

---
+++ 0x03-react_props/task_5/dashboard/src/Notifications/Notifications.test.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import Notifications from './Notifications';
+import { getLatestNotification } from '../utils/utils';
+
+const wrapper = shallow(<Notifications />);
+
+it('renders without crashing', () => {
+  shallow(<Notifications />);
+});
+
+it('renders NotificationItem', () => {
+  const nItem = wrapper.find('NotificationItem');
+  expect(nItem).toBeDefined();
+  expect(nItem.first().html()).toEqual(
+    '<li data-notification-type="default">New course available</li>'
+  );
+});
+
+it('renders the <p>', () => {
+  expect(
+    wrapper.containsMatchingElement(<p>Here is the list of notifications</p>)
+  ).toBeTruthy();
+});
+
+it('empty', () => {
+  const wrapper = shallow(
+    <Notifications displayDrawer listNotifications={listNotifications} />
+  );
+  expect(wrapper.exists());
+  const nItem = wrapper.find('NotificationItem');
+  expect(nItem).toHaveLength(1);
+  expect(nItem.html()).toEqual(
+    '<li data-notification-type="default">No new notification for now</li>'
+  );
+});
+
+it('without listNotifications', () => {
+  const wrapper = shallow(<Notifications displayDrawer />);
+  const nItem = wrapper.find('NotificationItem');
+  expect(nItem).toHaveLength(1);
+  expect(nItem.html()).toEqual(
+    '<li data-notification-type="default">No new notification for now</li>'
+  );
+});
