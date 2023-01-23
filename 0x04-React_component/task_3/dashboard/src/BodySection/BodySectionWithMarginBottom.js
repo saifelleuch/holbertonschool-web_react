6635c53cddd8daeb893a343c4aabd42b3b173242
saifelleuch <sa2014if@gmail.com>
2023-01-23 20:27:14 +0100

    another one

--- 0x04-React_component/task_3/dashboard/src/BodySection/BodySectionWithMarginBottom.js
+++ 0x04-React_component/task_3/dashboard/src/BodySection/BodySectionWithMarginBottom.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import BodySection from './BodySection';
-import './BodySectionWithMarginBottom.css';
 
 class BodySectionWithMarginBottom extends Component {
   constructor(props) {
