6360ed871395226f98bfc0f8ed5df8d9184a1e24
saifelleuch <sa2014if@gmail.com>
2023-01-23 14:16:03 +0100

    another one

--- 0x04-React_component/task_1/dashboard/src/App/App.test.js
+++ 0x04-React_component/task_1/dashboard/src/App/App.test.js
@@ -40,3 +40,12 @@ it('isLoggedIn is true', () => {
   expect(wrapper.find('Login')).toHaveLength(0);
   expect(wrapper.find('CourseList')).toHaveLength(1);
 });
+
+it('logOut', () => {
+  const logOut = jest.fn(() => undefined);
+  const wrapper = shallow(<App logOut={logOut} />);
+  const alert = jest.spyOn(global, 'alert');
+  expect(alert);
+  expect(logOut);
+  jest.restoreAllMocks();
+});
