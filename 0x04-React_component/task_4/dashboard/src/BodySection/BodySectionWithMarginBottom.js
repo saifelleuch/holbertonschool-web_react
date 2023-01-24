477362062750ce0f09621c6c06eda7db3d570116
saifelleuch <sa2014if@gmail.com>
2023-01-24 15:11:35 +0100

    another one

---
+++ 0x04-React_component/task_4/dashboard/src/BodySection/BodySectionWithMarginBottom.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import BodySection from './BodySection';
+
+class BodySectionWithMarginBottom extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className='bodySectionWithMargin'>
+        <BodySection {...this.props} />
+      </div>
+    );
+  }
+}
+
+BodySectionWithMarginBottom.defaultProps = {
+  title: '',
+};
+
+BodySectionWithMarginBottom.propTypes = {
+  title: PropTypes.string,
+};
+
+export default BodySectionWithMarginBottom;
