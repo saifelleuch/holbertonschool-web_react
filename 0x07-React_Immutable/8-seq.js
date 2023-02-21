ceb72c7e132a6256bb0a2f1397c93c81e735beb6
saifelleuch <sa2014if@gmail.com>
2023-02-21 23:16:18 +0100

    another one

---
+++ 0x07-React_Immutable/8-seq.js
@@ -0,0 +1,21 @@
+import { Seq } from 'immutable';
+
+const printBestStudents = (object) => {
+  const seq = Seq(object);
+
+  const getThestudent = seq.filter((value) => value.score > 70);
+
+  const theStudent = getThestudent.toJS();
+
+  const formatName = (name) => name.charAt(0).toUpperCase() + name.slice(1);
+
+  Object.keys(student).map((key) => {
+    theStudent[key].firstName = formatName(theStudent[key].firstName);
+    theStudent[key].lastName = formatName(theStudent[key].lastName);
+    return theStudent[key];
+  });
+
+  console.log(theStudent);
+};
+
+export default printBestStudents;
